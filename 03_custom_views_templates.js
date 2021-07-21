6de1a84a1d951186dc0d8e5de577a6c7214a2a8f
frankatimm <83537293+frankatimm@users.noreply.github.com>
2021-07-21 13:16:28 +0200

    RT feedback for incorrect answers aswell

--- 03_custom_views_templates.js
+++ 03_custom_views_templates.js
@@ -65,7 +65,7 @@ custom_views.keypress_FIT_practice = function(config) {
                         correctness = "incorrect";
                         // show feedback
                         $(".magpie-view-stimulus").addClass("magpie-invisible");
-                        $('#feedback').text('Incorrect!');
+                        $('#feedback').text('Incorrect! You reacted within ' + RT + ' ms.');
                     }
 
                     const trial_data = {
@@ -194,7 +194,7 @@ custom_views.keypress_FIT_main = function(config) {
                         correctness = "incorrect";
                         // show feedback
                         $(".magpie-view-stimulus").addClass("magpie-invisible");
-                        $('#feedback').text('Incorrect!');
+                        $('#feedback').text('Incorrect! You reacted within ' + RT + ' ms.'');
                     }
 
                     const trial_data = {
