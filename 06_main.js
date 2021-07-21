c7022dc2c23636585f9fa1b96ab4cdf25cb5fcd8
frankatimm <83537293+frankatimm@users.noreply.github.com>
2021-07-21 18:23:30 +0200

    Implementation of two Groups A and B

--- 06_main.js
+++ 06_main.js
@@ -1,5 +1,22 @@
 //initialize and configure experiment using magpieInit
 
+//two different groups
+    //groupA:
+        //Practice: conjunction, feature
+        //main: conj. conj. feat. conj. feat. feature
+    //groupB:
+        //Practice: feature, conj.
+        //main: feat. feat. conj. feat. conj. conj.
+
+//assign participants randomly
+
+const mapping = _.sample(["GroupA", "GroupB"]);
+
+//different view sequence depending on group assignment
+//GroupA:
+
+if(mapping == "GroupA"){
+
 $("document").ready(function() {
     // prevent scrolling when space is pressed
     window.onkeydown = function(e) {
@@ -15,18 +32,44 @@ $("document").ready(function() {
     window.magpie_monitor = magpieInit({
         // You have to specify all views you want to use in this experiment and the order of them
         views_seq: [
+            //introduction
             intro,
             dominant_hand,
             instructions,
+            //practice trials
+            //conjunction
             instructions_conjunction,
             practice_conjunction,
+            //feature
             instructions_feature,
             practice_feature,
+            //main trials
             main_instructions,
+            //conjunction
+            instructions_conjunction,
             main_conjunction,
             after_block,
+            //conjunction
+            instructions_conjunction,
+            main_conjunction,
+            after_block,
+            //feature
+            instructions_feature,
             main_feature,
             after_block,
+            //conjunction
+            instructions_conjunction,
+            main_conjunction,
+            after_block,
+            //feature
+            instructions_feature,
+            main_feature,
+            after_block,
+            //feature
+            instructions_feature,
+            main_feature,
+            after_block,
+            //post test and thanks
             post_test,
             thanks
         ],
@@ -57,3 +100,91 @@ $("document").ready(function() {
         }
     });
 });
+}
+
+if(mapping == "GroupB"){
+
+$("document").ready(function() {
+    // prevent scrolling when space is pressed
+    window.onkeydown = function(e) {
+        if (e.keyCode === 32 && e.target === document.body) {
+            e.preventDefault();
+        }
+    };
+
+    // calls magpieInit
+    // in debug mode this returns the magpie-object, which you can access in the console of your browser
+    // e.g. >> window.magpie_monitor or window.magpie_monitor.findNextView()
+    // in all other modes null will be returned
+    window.magpie_monitor = magpieInit({
+        // You have to specify all views you want to use in this experiment and the order of them
+        views_seq: [
+          //introduction
+          intro,
+          dominant_hand,
+          instructions,
+          //practice trials
+          //feature
+          instructions_feature,
+          practice_feature,
+          //conjunction
+          instructions_conjunction,
+          practice_conjunction,
+          //main trials
+          main_instructions,
+          //feature
+          instructions_feature,
+          main_feature,
+          after_block,
+          //feature
+          instructions_feature,
+          main_feature,
+          after_block,
+          //conjunction
+          instructions_conjunction,
+          main_conjunction,
+          after_block,
+          //feature
+          instructions_feature,
+          main_feature,
+          after_block,
+          //conjunction
+          instructions_conjunction,
+          main_conjunction,
+          after_block,
+          //conjunction
+          instructions_conjunction,
+          main_conjunction,
+          after_block,
+          //post test and thanks
+          post_test,
+          thanks
+      ],
+
+        // Here, you can specify all information for the deployment
+        deploy: {
+            experimentID: "256",
+            serverAppURL: "https://magpie-demo.herokuapp.com/api/submit_experiment/",
+            // Possible deployment methods are:
+            // "debug" and "directLink"
+            // As well as "MTurk", "MTurkSandbox" and "Prolific"
+            deployMethod: "debug",
+            contact_email: "franka.timm00@gmail.com",
+            prolificURL: "https://app.prolific.ac/submissions/complete?cc=SAMPLE1234"
+        },
+        // Here, you can specify how the progress bar should look like
+        progress_bar: {
+            in: [
+                // list the view-names of the views for which you want a progress bar
+                practice_conjunction.name,
+                practice_feature.name,
+                main_conjunction.name,
+                main_feature.name,
+            ],
+             // Possible styles are "default", "separate" and "chunks"
+            style: "separate",
+            width: 100
+        }
+    });
+});
+}
