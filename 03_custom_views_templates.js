ab71aa28b83b75350c63a953375367ee1a20cbb6
frankatimm <83537293+frankatimm@users.noreply.github.com>
2021-07-21 20:39:01 +0200

    Add files via upload

--- 03_custom_views_templates.js
+++ 03_custom_views_templates.js
@@ -239,8 +239,8 @@ custom_views.keypress_FIT_main = function(config) {
 
                     magpie.trial_data.push(trial_data);
                     $("body").off("keydown", handleKeyPress);
-                    setTimeout(magpie.findNextView, 1500); // delay to accomodate feedback
-        
+                    setTimeout(magpie.findNextView(), 1500); // delay to accomodate feedback
+
                 }
             };
 
