8fe21f9f84540e3c403292df52c206a8b91d70c1
schijohanna <83468749+schijohanna@users.noreply.github.com>
2021-07-21 12:03:26 +0200

    Add files via upload
    addition of hook after the pause

--- 05_views.js
+++ 05_views.js
@@ -46,9 +46,9 @@ const main_instructions = magpieViews.view_generator("instructions", {
   name: 'main_instructions',
   title: 'Instructions',
   text: `So far so good!
-  You have now finished the practice phase and hopefully well prepared for the main trials.
-  They are identical in format and content.
-  In order to proceed, press the button below.`,
+  You now finished the practice phase and are hopefully well prepared for the main
+  part of the experiment. The main trials are identical in form and content
+  as the practice trials. In order to proceed, press the button below.`,
   buttonText: 'proceed to main trails'
 });
 
@@ -67,16 +67,17 @@ const instructions_feature = magpieViews.view_generator("instructions", {
   name: 'instructions_conjunction',
   title: 'Instructions',
   text: `Now you are asked to search for two targets, each defined by a
-  different single feature: a color (blue) and a shape (S). Hence, the target you are supposed to find is either a blue X, blue T (color) or a brown or green S (shape).
+  different single feature: a color (blue) and a shape (S). Hence, the target you are supposed to find is either a blue X,
+  blue T (color) or a brown or green S (shape).
   In the presence of the target press "L", otherwise press "S".`,
-  buttonText: 'start the experiment'
+  buttonText: 'Start the experiment'
 });
 // In the post test questionnaire you can ask your participants addtional questions
 const post_test = magpieViews.view_generator("post_test", {
   trials: 1,
   name: 'post_test',
   title: 'Additional information',
-  text: ' Good job! You nearly done with the experiment. The last step is to answer the following questions.'
+  text: 'Good job! You are almost done with the experiment. The last step is to answer the following questions.'
 
   // buttonText: 'continue',
   // dominant_hand: 'dominant hand',
@@ -131,14 +132,16 @@ const practice_conjunction = custom_views.keypress_FIT_practice({
     name: 'practice',
     trial_type: 'practice',
     pause: 250,
-    after_pause: get_ready,
+    hook: {
+      after_pause: get_ready,
+    },
     //"get ready!" on white screen
     fix_duration: 1000,
     data: _.shuffle(practice_conjunction_trials_info.key_press),
     key1: "s",
     key2: "l",
-    s: "negative",
-    l: "positive",
+    s: "incorrect",
+    l: "correct",
 });
 
 const practice_feature = custom_views.keypress_FIT_practice({
@@ -146,28 +149,32 @@ const practice_feature = custom_views.keypress_FIT_practice({
     name: 'practice',
     trial_type: 'practice',
     pause: 250,
-    after_pause: get_ready,
+    hook: {
+      after_pause: get_ready,
+    },
     //"get ready!" on white screen
     fix_duration: 1000,
     data: _.shuffle(practice_feature_trials_info.key_press),
     key1: "s",
     key2: "l",
-    s: "negative",
-    l: "positive",
+    s: "incorrect",
+    l: "correct",
 });
 const main_feature = custom_views.keypress_FIT_main({
     trials: 64,
     name: 'main',
     trial_type: 'main',
     pause: 250,
-    after_pause: get_ready,
+    hook: {
+      after_pause: get_ready,
+    },
     //"get ready!" on white screen
     fix_duration: 1000,
     data: _.shuffle(main_feature_trials_info.key_press),
     key1: "s",
     key2: "l",
-    s: "negative",
-    l: "positive",
+    s: "incorrect",
+    l: "correct",
 });
 
 
@@ -176,12 +183,14 @@ const main_conjunction = custom_views.keypress_FIT_main({
     name: 'main',
     trial_type: 'main',
     pause: 250,
-    after_pause: get_ready,
+    hook: {
+      after_pause: get_ready,
+    },
     //"get ready!" on white screen
     fix_duration: 1000,
     data: _.shuffle(main_conjunction_trials_info.key_press),
     key1: "s",
     key2: "l",
-    s: "negative",
-    l: "positive",
+    s: "incorrect",
+    l: "correct",
 });
