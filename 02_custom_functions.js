c2c51881b419a14639e8b6f90cf109f26d73fcfb
frankatimm <83537293+frankatimm@users.noreply.github.com>
2021-07-22 12:00:56 +0200

    change alert to text

--- 02_custom_functions.js
+++ 02_custom_functions.js
@@ -65,6 +65,6 @@ check_response = function(data, next) {
 
 // compares the chosen answer to the value of `option1`
 get_ready = function(data, next){
-    alert('Get ready! Press a key!');
+    text('Get ready! Press the space bar or enter to continue!');
     next();
 }
