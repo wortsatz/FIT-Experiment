ee324f45b496674afa27f845544b3b219e5d6db1
frankatimm <83537293+frankatimm@users.noreply.github.com>
2021-08-01 14:55:47 +0200

    add settimeout in main function

--- 03_custom_views_templates.js
+++ 03_custom_views_templates.js
@@ -242,7 +242,7 @@ custom_views.keypress_FIT_main = function(config) {
 
                   magpie.trial_data.push(trial_data);
                   $("body").off("keydown", handleKeyPress);
-                  magpie.findNextView; // delay to accomodate feedback
+                  setTimeout(magpie.findNextView, 1500); // delay to accomodate feedback
               }
           };
 
